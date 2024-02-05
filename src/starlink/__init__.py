2577f9a27c640175dd11b48c6d63210b7492993b
qbarnes <quinton.barnes68@gmail.com>
2024-02-05 16:58:07 -0600

    made _starlinkConfig publicly available

--- src/starlink/__init__.py
+++ src/starlink/__init__.py
@@ -8,19 +8,9 @@ from pathlib import Path
 from .config import _importConfig
 
 _currentWorkingDirectory = os.getcwd()
-# _tmpPath = Path(_currentWorkingDirectory).joinpath('config/starlink-config.toml')
 _tmpPath = Path(_currentWorkingDirectory).joinpath('starlink-config.toml')
-# if _tmpPath.exists():
-#     _configPath = _tmpPath
-# else:
-#     # todo: change this to actual package name when this is installed
-#     with files('starlink').joinpath('config/starlink-config.toml') as file:
-#         _configPath = file
-#
-# with open(_configPath, 'rb') as toml:
-#     _starlinkConfig = tomllib.load(toml)
 _overridePath = _tmpPath if _tmpPath.exists() else None
-_starlinkConfig = _importConfig(_overridePath)
+starlinkConfig = _importConfig(_overridePath)
 
 # check if the updated time exists in config dict and if they don't match update from server
 
