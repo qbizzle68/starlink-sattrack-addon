279b0cb00625baeaee1a8d8e748cb4fc9f4baf9f
qbarnes <quinton.barnes68@gmail.com>
2024-02-25 02:50:53 -0600

    added import checking of dependencies

--- src/starlink/__init__.py
+++ src/starlink/__init__.py
@@ -1,5 +1,19 @@
 import os
 
+_dependencies = ('sattrack', 'pyevspace', 'requests')
+_missingImports = []
+for _package in _dependencies:
+    try:
+        __import__(_package)
+    except ImportError as e:
+        _missingImports.append(f'{_package}: {e}')
+
+if _missingImports:
+    error = 'Unable to import dependencies:\n' + '\n'.join(_missingImports)
+    raise ImportError(error)
+
+del _dependencies, _missingImports, _package
+
 from pathlib import Path
 
 from .models.satellites import *
